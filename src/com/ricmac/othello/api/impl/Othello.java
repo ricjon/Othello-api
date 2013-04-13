be4d20bb929fe15f11033e1c2e6d1b1504d2a2c9
ricjon <rickard.jonsson@yahoo.se>
2013-04-13 21:39:23 +0200

    Generate a random othello olay board

--- src/com/ricmac/othello/api/impl/Othello.java
+++ src/com/ricmac/othello/api/impl/Othello.java
@@ -1,5 +1,7 @@
 package com.ricmac.othello.api.impl;
 
+import java.util.Random;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,7 +14,19 @@ public class Othello {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/getstring")
 	public String getEmployee() {
-		return "{\"board\" : \"the board\"}";
+		
+		return "{\"board\" :" + getRandomBoard() + " }";
+		//return "{\"board\" : \"rrwrerwyrryyyyyyyyrrrerrryyyyyyrr\"}";
 	}
 	
+	private String getRandomBoard() {
+		char[] othellomarker = {'r', 'y', 'e'};
+		String board = "\"";
+		for (int i = 0; i < 64; i++ ) {
+			board = board + othellomarker[new Random().nextInt(3)];
+		}
+		board = board + "\"";
+		return board;
+	}
+ 	
 }
